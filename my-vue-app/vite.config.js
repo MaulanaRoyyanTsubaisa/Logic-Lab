cdde4ea224abb95d443d9493d803b38b6a5d0d77
Muhammad Rahman <hrepunzel@gmail.com>
2025-03-10 10:33:59 +0700

    🔁 refactore (vite.config.js) : renamed file index.html to LogicLab.html

--- my-vue-app/vite.config.js
+++ my-vue-app/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(),tailwindcss()],
+  plugins: [vue(), tailwindcss()],
   build: {
     rollupOptions: {
-      logiclab: "index.html",
+      logiclab: "my-vue-app/LogicLab.html",
     },
   },
 });
